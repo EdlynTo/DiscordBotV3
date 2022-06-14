fff644f4a00810158eb521d419c019169c473c6f
Edlyn To <68762251+EdlynTo@users.noreply.github.com>
2022-06-14 00:56:55 +0000

    New code

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,5 @@
 import discord
 import os
-#import pynacl
-#import dnspython
-import server
-from discord.ext import commands
-
-# bot = commands.Bot(command_prefix="$")
-# TOKEN = os.getenv("DISCORD_TOKEN")
-
-# server.server()
-# bot.run(TOKEN)
-
 import random
 import pyjokes
 from keep_alive import keep_alive
